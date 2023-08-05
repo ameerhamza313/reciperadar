44b6ad83d2ea6fe0cc5f84ab75d96b7556f72e79
Ameerhamza Sanadi <ameerhamzasanadi@gmail.com>
2023-08-05 13:01:52 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
-// components/Navbar.js
-
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const CustomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">RecipeRadar</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">RecipeRadar</Navbar.Brand> {/* Use Link to navigate to Home */}
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/favorites">Favorite Recipes</Nav.Link>
-            {/* Add more links for other pages as needed */}
+            {/* Use Link to navigate to Home */}
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            {/* Use Link to navigate to Favorite Recipes */}
+            <Nav.Link as={Link} to="/favorites">Favorite Recipes</Nav.Link>
+            {/* Add more Nav.Links for other pages */}
           </Nav>
         </Navbar.Collapse>
       </Container>
